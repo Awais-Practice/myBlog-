e6755985baba209fe6acb4f16efd963ae01b47dd
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2021-02-07 23:30:07 +0500

    show tab contents based on dropdown selection

--- src/app/contentContainer/blog-topics/blog-topics.component.ts
+++ src/app/contentContainer/blog-topics/blog-topics.component.ts
@@ -4,27 +4,21 @@ interface Topics {
   viewValue: string;
 }
 
-
 @Component({
   selector: 'app-blog-topics',
   templateUrl: './blog-topics.component.html',
-  styleUrls: ['./blog-topics.component.scss']
+  styleUrls: ['./blog-topics.component.scss'],
 })
 export class BlogTopicsComponent implements OnInit {
+  topic = 'home-0';
   topics: Topics[] = [
-    {value: 'home-0', viewValue: 'Home'},
-    {value: 'profile-1', viewValue: 'Profile'},
-    {value: 'messgesa-2', viewValue: 'Messages'},
-    {value: 'settings-2', viewValue: 'Settings'}
+    { value: 'home-0', viewValue: 'Home' },
+    { value: 'profile-1', viewValue: 'Profile' },
+    { value: 'messgesa-2', viewValue: 'Messages' },
+    { value: 'settings-2', viewValue: 'Settings' },
   ];
 
+  constructor() {}
 
-
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
